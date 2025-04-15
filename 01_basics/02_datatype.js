2650c2187daab92a021eba4096d6a115515d484f
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-04-15 17:50:12 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node js not browser
+
+console.log(3+3) 
+
+console.log("shivansh")
+
+
+let name ="shivansh"
+let age = 18
+let isLoggedIn = false;
+
+/* primitive datatype*/ 
+// number=> 2 to power 53
+// bigint
+//string => use double quaots ""
+
+//boolean => true/false
+//null=>standalone value
+//undefined => value not assign yet
+// symbol =>use to find uniqueness
+
+
+// object
+
+console.log(typeof "shivansh")
+console.log(typeof null)  // object 
+console.log(typeof undefined) // undefined
