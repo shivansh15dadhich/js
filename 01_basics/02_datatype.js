49cc4f451ddb42555c240f24d645082710fb25f3
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-04-18 10:30:43 +0000

    data type

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,15 +1,15 @@
-"use strict"; //treat all JS code as newer version
+// "use strict"; //treat all JS code as newer version
 
 // alert("hello") //we are using node js not browser
 
-console.log(3+3) 
+// console.log(3+3) 
 
-console.log("shivansh")
+// console.log("shivansh")
 
 
-let name ="shivansh"
-let age = 18
-let isLoggedIn = false;
+// let name ="shivansh"
+// let age = 18
+// let isLoggedIn = false;
 
 /* primitive datatype*/ 
 // number=> 2 to power 53
@@ -24,6 +24,30 @@ let isLoggedIn = false;
 
 // object
 
-console.log(typeof "shivansh")
-console.log(typeof null)  // object 
-console.log(typeof undefined) // undefined
+// console.log(typeof "shivansh")
+// console.log(typeof null)  // object 
+// console.log(typeof undefined) // undefined
+
+
+// *******************************javascript.info code***************************
+                     
+let name = "john"
+let admin = name
+console.log(name);
+console.log(admin);
+
+
+let PlanetName ="earth"
+console.log(PlanetName);
+let CurrentVisitor
+CurrentVisitor = "user"
+console.log(CurrentVisitor);
+
+const BIRTHDAY = '18.04.1982'; 
+
+const AGE = someCode(BIRTHDAY); // somecode calculate 
+
+console.log(AGE);
+
+
+
