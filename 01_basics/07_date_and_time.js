c98ad92129b73e10d728a534fc041fbca2afd3f6
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-04-18 13:10:17 +0000

    time and date

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,40 @@
+// let myDate = new Date()
+
+// console.log(myDate);
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 3 ,18)
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+// let mytimeStamp = Date.now()
+
+// console.log(mytimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor( Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+
+newDate.toLocaleString('default',{weekday:"long" })
+
+
+
+
+
+
+
+
+
+
+
