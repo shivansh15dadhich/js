674043b22765ceceec49fceaf45bb95468a0c6d0
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-04-18 11:56:24 +0000

    number and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance.toString());
+
+console.log(balance.toFixed(3));
+  
+console.log(Math);
+
+console.log(Math.random());
+
+const min = 10
+const max = 20
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+ 
+
+
