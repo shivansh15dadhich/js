670b384a4ec2866488fa427f98b346d4619216e4
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-04-20 06:29:46 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,48 @@
+// array 
+
+const myArr = [0,1,2,3,4,5]
+
+// const myHeros = [name1,name2,nam3]
+
+const myArr2 = new Array(1,2,3,4,5,6)
+// console.log(myArr[1]);
+
+// array method 
+ 
+// myArr.push(6)
+//  myArr.push(7)
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+
+// console.log(myArr);
+//  myArr.pop() 
+
+// slice, splice 
+console.log("A",myArr);
+
+const mya1 = myArr.slice(1,3)
+
+console.log(mya1);
+
+console.log("B",myArr);
+
+
+const mya2 = myArr.splice(1,3)
+console.log(mya2);
+
+console.log("c",myArr);
+
+
+
