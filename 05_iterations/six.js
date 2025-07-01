a0765ea89431fa09fd66a0f0ae9fa9d75946973a
shivansh15dadhich <shivanshdadhich06@gmail.com>
2025-07-01 19:32:21 +0000

    map

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,9 +1,25 @@
-const coding =[1,2,3,4,5,6,7,8,9,10]
+// const coding =[1,2,3,4,5,6,7,8,9,10]
 
-const values = coding.forEach((item) =>{
-    return item
+// const values = coding.forEach((item) =>{
+//     return item
+// })
+
+// console.log(values) // undefined;
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums =  myNums.filter((item) => {
+//     return item > 5;
+// })
+
+
+
+
+const newNums =[]
+myNums.array.forEach(num => {
+    if(num>4)
+    {newNums.push(num)}
 })
-
-console.log(values) // undefined;
-
+console.log(newNums);
 
